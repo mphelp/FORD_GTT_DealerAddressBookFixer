c6ceaf74a07a6a81d722e7d2ba9b9218cea0944d
Phelps <mphelp20@ford.com>
2018-06-29 16:11:53 -0400

    still figuring this out

--- cityParseGlobal.py
+++ cityParseGlobal.py
@@ -15,33 +15,17 @@ import time
 import openpyxl
 
 # Vars
-globalResultsFile = 'resultsGlobal.txt'
-incompleteAddrExcel = 'dependencies/20180620 GTT Dealers with Incomplete address.xlsx'
 
+incompleteAddrExcel = 'dependencies/20180620 GTT Dealers with Incomplete address.xlsx'
 incompleteSheetName = 'Address Data city is inappropri'
 approvedGTNAddrExcel = 'dependencies/GTNexus_CityList_20180208.xlsx'
-debugAll = False
-hasOffSet = False
 
-# Source, I/O Read Write
-myTimer = Timer.Timer()
-myTimer.start('Excel Loading')
-incompleteAddrTable = pd.read_excel(pd.ExcelFile(incompleteAddrExcel), incompleteSheetName)
-approvedGTNAddrTable = pd.read_excel(approvedGTNAddrExcel)
-AddrInfoGlobal = AddrStats.AddrStats(resultsFile=globalResultsFile,debugAll=debugAll,hasOffset=hasOffSet)
-myTimer.end()
+# Instantiate
 
-# List Setup
-myTimer.start('List Setup')
-incompleteAddrCityList = [parse.convertToStr(getattr(addr, 'City'))
-                          for addr in incompleteAddrTable[['City']].itertuples()]
-approvedGTNAddrCityList = [parse.convertToStr(getattr(addr, 'City'))
-                           for addr in approvedGTNAddrTable[['City']].itertuples()]
-approvedGTNAddrCityListSimplified = [parse.remPunctuation(c.lower()) for c in approvedGTNAddrCityList]
-myTimer.end()
+
 
 # Begin iteration
-myTimer.start('Iteration')
+
 for possibleCity in incompleteAddrCityList:
     matchFound, correctCity = parse.cityStringParse(possibleCity, approvedGTNAddrCityList,
                                                     approvedGTNAddrCityListSimplified)
@@ -55,3 +39,16 @@ myTimer.end()
 
 # Display results
 AddrInfoGlobal.dispCityResults()
+
+##### TO DO
+'''
+Classes:
+FinalAddrExcel - write all columns (new and old) to excel file
+                - auto fill excel file
+GTNAddressLookup - load and store all accepted address details
+                  - lookup function to see if city/country is in dataframe
+IncompleteGTTAddr - load and store all incomplete GTT address details
+                   - store a log of successes/failures
+                   - record current position of parsing
+
+'''
--- lib/GTNAddressLookup.py
+++ lib/GTNAddressLookup.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from lib import parse
 class GTNAddressLookup:
 
     def __init__(self, approvedAddressesFile='dependencies/GTNexus_CityList_20180208.xlsx'):
@@ -8,5 +9,7 @@ class GTNAddressLookup:
         self.approvedAddressesDataFrame = pd.read_excel(self.approvedAddressesFile)
 
     def selectKeyColumnsFromDataFrame(self):
-        self.approvedCities    =
-        self.approvedCountries =
+        self.approvedCities    = [parse.convertToStr(getattr(addr, 'City')) for addr in
+                                  self.approvedAddressesDataFrame[['City']].itertuples()]
+        self.approvedCountries = [parse.convertToStr(getattr(addr, 'Country Name')) for addr in
+                                  self.approvedAddressesDataFrame[['Country Name']].itertuples()]
