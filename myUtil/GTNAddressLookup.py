7c5bfbc75c4265bafc07d5d3b5931126ae4e4c76
Phelps <mphelp20@ford.com>
2018-07-03 10:07:14 -0400

    modified GTNAddressesLookup. lookup looks good

--- myUtil/GTNAddressLookup.py
+++ myUtil/GTNAddressLookup.py
@@ -16,9 +16,29 @@ class GTNAddressLookup:
                                      in self.approvedCities]
         self.approvedCountriesSimple = [parse.remPunctuation(c[0].lower()) for c
                                         in self.approvedCountries]
-        print(self.approvedCitiesSimple)
-        print('{}'.format('New York' in self.approvedCities))
-        print('Gabon' in self.approvedCountries)
+        print(self.approvedCities[2])
+        print(self.approvedCitiesSimple[2])
+        print('Encamp is in approved cities : {}'.format('Encamp' in self.approvedCities))
+        print('encamp is in simple approved : {}'.format('encamp' in self.approvedCitiesSimple))
+
+        # Quick tests ...
+
+        #cityMatchesList = [self.approvedCities[index].item(0) for index, citySimple in
+                           #enumerate(self.approvedCitiesSimple) if
+                           #citySimple in 'stratford upon avon' and len(citySimple) >= 3]
+        #print(cityMatchesList)
+        #print('Len: {}'.format(len(cityMatchesList)))
+        #returnWord = max(cityMatchesList, key=len)
+        #print('Returns: {}'.format(returnWord))
+        #print('Return type: {}'.format(type(returnWord)))
+        #print(returnWord.item(0))
+
+        # print('Type: {}'.format(type(returnWord.item(0))))
+
+        ### Debugging Purposes:
+        #print(self.approvedCitiesSimple)
+        #print('{}'.format('New York' in self.approvedCities))
+        #print('Gabon' in self.approvedCountries)
         #self.approvedCitiesSimple = np.ndarray([parse.remPunctuation(c.lower() for c in
                                                 #self.approvedCities)])
         #for i in self.approvedAddressesDataFrame:
@@ -35,10 +55,11 @@ class GTNAddressLookup:
   #  self.approvedCitiesSimple = [parse.remPunctuation(city.lower()) for city in self.approvedCities]
       #  self.approvedCountriesSimple = [parse.remPunctuation(country.lower()) for country in self.approvedCountries]
 
-    def cityFoundViaLookup(self, addr, cityList, cityListSimplified):
+    def cityFoundViaLookup(self, addr):
         addrSimplified = parse.remPunctuation(addr.lower())
-        matchesList = [cityList[index] for index, citySimple in enumerate(cityListSimplified) if
-                       citySimple in addrSimplified and len(citySimple) >= 4]
+        matchesList = [self.approvedCities[index].item(0) for index, citySimple in
+                       enumerate(self.approvedCitiesSimple) if
+                       citySimple in addrSimplified and len(citySimple) >= 3]
         if len(matchesList) == 0:
             return None
         else:
