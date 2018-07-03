9baf26736062bc1dafabb15123c39694a5da29fc
Phelps <mphelp20@ford.com>
2018-07-03 13:26:50 -0400

    Created some successful unittest. base algorithm functional

--- myUtil/GTNAddressLookup.py
+++ myUtil/GTNAddressLookup.py
@@ -1,46 +1,10 @@
 from myUtil import parse
 
 class GTNAddressLookup:
+    """This class contains the central algrorithms for address parsing."""
 
-        # Quick tests ...
-
-        #print(self.approvedCities[2])
-        #print(self.approvedCitiesSimple[2])
-        #print('Encamp is in approved cities : {}'.format('Encamp' in self.approvedCities))
-        #print('encamp is in simple approved : {}'.format('encamp' in self.approvedCitiesSimple))
-
-        #cityMatchesList = [self.approvedCities[index].item(0) for index, citySimple in
-                           #enumerate(self.approvedCitiesSimple) if
-                           #citySimple in 'stratford upon avon' and len(citySimple) >= 3]
-        #print(cityMatchesList)
-        #print('Len: {}'.format(len(cityMatchesList)))
-        #returnWord = max(cityMatchesList, key=len)
-        #print('Returns: {}'.format(returnWord))
-        #print('Return type: {}'.format(type(returnWord)))
-        #print(returnWord.item(0))
-
-        # print('Type: {}'.format(type(returnWord.item(0))))
-
-        ### Debugging Purposes:
-        #print(self.approvedCitiesSimple)
-        #print('{}'.format('New York' in self.approvedCities))
-        #print('Gabon' in self.approvedCountries)
-        #self.approvedCitiesSimple = np.ndarray([parse.remPunctuation(c.lower() for c in
-                                                #self.approvedCities)])
-        #for i in self.approvedAddressesDataFrame:
-            #print(f'{i}')
-        #self.approvedCountries = [parse.convertToStr(getattr(addr, 'Country Name')) for addr in
-                                #  approvedAddressesDataFrame[['Country Name']].itertuples()]
-        #self.approvedCountries = [type(country) for country in
-                                  #self.approvedAddressesDataFrame[['Country Name']].tolist()]
-        #print(self.approvedCountries)
-        #print('Andorra' in self.approvedCountries)
-       # self.approvedCities    = [parse.convertToStr(getattr(addr, 'City')) for addr in
-                        #          self.approvedAddressesDataFrame[['City']].itertuples()]
-
-  #  self.approvedCitiesSimple = [parse.remPunctuation(city.lower()) for city in self.approvedCities]
-      #  self.approvedCountriesSimple = [parse.remPunctuation(country.lower()) for country in self.approvedCountries]
-
+    # Lookup city default method: search for a match between
+    # source address line string and entry in GTN approved city/country address book
     def lookupCity(self, addr, addressBook):
         addrSimplified = parse.remPunctuation(addr.lower())
         matchesList = [addressBook.approvedCities[index].item(0) for index, citySimple in
--- test/test_GTNAddressLookup.py
+++ test/test_GTNAddressLookup.py
@@ -11,5 +11,23 @@ class TestGTNAddressLookup(TestCase):
         addressBook = ai.loadGTNApprovedAddressesCitiesAndCountries(df)
 
         l = GTNAddressLookup()
-        actualCity = l.lookupCity("89aeavLONDon--", addressBook)
-        self.assertEqual(actualCity, "London")
+        actual = l.lookupCity("89aeavLONDon--", addressBook)
+        self.assertEqual(actual, "London")
+
+    def test_lookupCityDifferentCity(self):
+        df = pd.DataFrame(data={'City': ['London','New Delphi'], 'Country Name': ['United Kingdom','India']})
+        ai = AddressImporter()
+        addressBook = ai.loadGTNApprovedAddressesCitiesAndCountries(df)
+
+        l = GTNAddressLookup()
+        actual = l.lookupCity("aev0.New.delphI.", addressBook)
+        self.assertEqual("New Delphi", actual)
+
+    def test_lookupCountry(self):
+        df = pd.DataFrame(data={'City': ['London','New Delphi','Oslo'],
+                                'Country Name': ['United Kingdom','India','Norway']})
+        ai = AddressImporter()
+        addressBook = ai.loadGTNApprovedAddressesCitiesAndCountries(df)
+        l = GTNAddressLookup()
+        actual = l.lookupCountry("-seNOrway", addressBook)
+        self.assertEqual("New Delphi", actual)
